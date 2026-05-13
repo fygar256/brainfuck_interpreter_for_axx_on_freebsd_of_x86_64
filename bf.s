5a278d60da8c63ba0b6ab5b0770cbf79322388f3
Taisuke Maekawa <fygar256@gmail.com>
2026-05-13 14:19:56 +0900

    Add files via upload

--- bf.s
+++ bf.s
@@ -1,163 +1,191 @@
 ; Brainfuck interpreter
-; FreeBSD x86_64
-; Pure syscall
+; FreeBSD x86_64 / Linux x86_64 (syscall番号を変更)
 ; Intel syntax (axx)
-
-; build:
-;   python3 axx.py x86_64.axx bf.s -o bf.o --osabi FreeBSD
-;   ld -o bf bf.o
+;
+; build (FreeBSD):
+;   python3 axx.py x86_64.axx bf.s -o bf.o --osabi FreeBSD && ld -o bf bf.o
+;
+; build (Linux):
+;   syscall番号を Linux 用 (下記参照) に変更してから:
+;   python3 axx.py x86_64.axx bf.s -o bf.o && ld -o bf bf.o
+;
+; ===== テープアクセスの方針 =====
+; tape_a: .equ tape::abs32 と定義し、
+; x86_64.axx の [!s\+r] パターンが 4バイト disp32 フィールドに
+; R_X86_64_32 (絶対32bitアドレス) リロケーションを生成する。
+;
+; これにより [symbol+register] SIBアドレッシングが使用でき、
+; r15 をテープインデックス (0オリジン) として扱える。
+;
+; レジスタ割り当て:
+;   r12 = fd (ファイルオープン時のみ)
+;   r13 = プログラム長 (バイト数)
+;   r14 = 命令ポインタ (prog_buf インデックス)
+;   r15 = テープインデックス (tape[r15] が現在セル)
 
 .global _start
 
-SYS_EXIT:  .equ   1
-SYS_READ:  .equ   3
-SYS_WRITE: .equ   4
-SYS_OPEN:  .equ   5
-SYS_CLOSE: .equ   6
+; --- FreeBSD syscall番号 ---
+; (Linux: EXIT=60, READ=0, WRITE=1, OPEN=2, CLOSE=3)
+SYS_EXIT:  .equ  1
+SYS_READ:  .equ  3
+SYS_WRITE: .equ  4
+SYS_OPEN:  .equ  5
+SYS_CLOSE: .equ  6
 
 .section .data
-usage:      .ascii "Usage: bf <file>",10
-usage_len:  .equ $$ - usage
+usage:     .ascii "Usage: bf <file>",10
+usage_len: .equ $$ - usage
 .endsection
 
 .section .bss
-tape:      .zero 65536
-prog_buf:  .zero 1048576
+tape:     .zero 65536
+prog_buf: .zero 1048576
 .endsection
+
+; SIB abs32 エイリアス:
+;   INCB/DECB/CMPB [tape_a+r15] が R_X86_64_32 リロケーションを生成する
+tape_a: .equ tape::abs32
 
 .section .text
 
 _start:
-    mov  r8, rdi               ; r8 = &argc
-    movd  eax, [r8]            ; eax = argc
-    cmpd  eax, 2
-    jge   _open_file
+    ; FreeBSD x86_64: rsp → argc, rsp+8 → argv[0], rsp+16 → argv[1]
+    mov  r8, rsp
+    movd eax, [r8]         ; eax = argc
+    cmpd eax, 2
+    jge  _open_file
+
+    ; 引数不足: 使い方を stderr に表示して終了
     mov rax, SYS_WRITE
-    mov rdi, 2                 ; stderr
-    lea rsi,[RIP+usage]
+    mov rdi, 2
+    lea rsi, [RIP+usage]
     mov rdx, usage_len
     syscall
-
     mov rax, SYS_EXIT
     mov rdi, 1
     syscall
 
 _open_file:
     mov rax, r8
-    add rax, 16                ; r8+8=argv[0], r8+16=argv[1]
-    mov rdi, [rax]             ; rdi = argv[1]
+    add rax, 16            ; &argv[1]
+    mov rdi, [rax]         ; argv[1] = ファイル名ポインタ
     mov rax, SYS_OPEN
-    xor rsi, rsi               ; O_RDONLY = 0
-    xor rdx, rdx               ; mode = 0
+    xor rsi, rsi           ; O_RDONLY = 0
+    xor rdx, rdx
     syscall
-    jc  exit_error
-    mov r12, rax               ; r12 = fd
+    jc  exit_error         ; FreeBSD: CF=1 はシステムコールエラー
+                           ; (Linux では: jl exit_error)
+    mov r12, rax           ; r12 = fd
 
-    ; ファイルを丸ごと読み込む
+    ; ファイルを prog_buf に一括読み込み
     mov rax, SYS_READ
     mov rdi, r12
     mov rsi, prog_buf
     mov rdx, 1048576
     syscall
-    mov r13, rax
+    mov r13, rax           ; r13 = プログラム長
 
     mov rax, SYS_CLOSE
     mov rdi, r12
     syscall
 
     ; 初期化
-    xor r14, r14
-    lea r15, [RIP+tape]
+    xor r14, r14           ; 命令ポインタ = 0
+    xor r15, r15           ; テープインデックス = 0
+
 main_loop:
     cmp r14, r13
     jge exit
 
+    ; 現在の BF 命令を al に読み込む
     mov rax, prog_buf
     add rax, r14
     movb al, [rax]
 
     cmpb al, '>'
-    je op_inc_ptr
+    je   op_inc_ptr
     cmpb al, '<'
-    je op_dec_ptr
+    je   op_dec_ptr
     cmpb al, '+'
-    je op_inc_val
+    je   op_inc_val
     cmpb al, '-'
-    je op_dec_val
+    je   op_dec_val
     cmpb al, '.'
-    je op_output
+    je   op_output
     cmpb al, ','
-    je op_input
+    je   op_input
     cmpb al, '['
-    je op_loop_start
+    je   op_loop_start
     cmpb al, ']'
-    je op_loop_end
+    je   op_loop_end
 
 next:
     inc r14
     jmp main_loop
 
-; ----- operations -----
+; ===== Brainfuck 命令実装 =====
 
 op_inc_ptr:
-    inc r15
+    inc r15                ; > : テープポインタを+1
     jmp next
 
 op_dec_ptr:
-    dec r15
+    dec r15                ; < : テープポインタを-1
     jmp next
 
 op_inc_val:
-    incb [r15]
+    incb [tape_a+r15]      ; + : 現在セルをインクリメント [SIB R_X86_64_32]
     jmp next
 
 op_dec_val:
-    decb [r15]
+    decb [tape_a+r15]      ; - : 現在セルをデクリメント [SIB R_X86_64_32]
     jmp next
 
 op_output:
-    mov rax, SYS_WRITE
-    mov rdi, 1                 ; stdout
-    mov rsi, r15
+    ; . : write(1, &tape[r15], 1)
+    ; rsi = tape の絶対アドレス + r15 (インデックス)
+    mov rsi, tape_a        ; rsi = tape の絶対アドレス (imm64, R_X86_64_64)
+    add rsi, r15
     mov rdx, 1
+    mov rax, SYS_WRITE
+    mov rdi, 1
     syscall
     jmp next
 
 op_input:
-    mov rax, SYS_READ
-    mov rdi, 0                 ; stdin
-    mov rsi, r15
+    ; , : read(0, &tape[r15], 1)
+    mov rsi, tape_a
+    add rsi, r15
     mov rdx, 1
+    mov rax, SYS_READ
+    xor rdi, rdi
     syscall
     cmp rax, 0
-    jle exit
+    jle exit               ; EOF またはエラー
     jmp next
 
-; ----- loops -----
-
 op_loop_start:
-    cmpb [r15], 0
-    jne next
+    ; [ : 現在セルが 0 なら対応する ']' の直後へジャンプ
+    cmpb [tape_a+r15], 0   ; [SIB R_X86_64_32]
+    jne  next
 
-    mov rcx, 1
+    mov rcx, 1             ; ネスト深度
 .find_end:
     inc r14
     cmp r14, r13
     jge exit
-
     mov rax, prog_buf
     add rax, r14
     movb al, [rax]
     cmpb al, '['
-    je .inc_depth
+    je   .inc_depth
     cmpb al, ']'
-    je .dec_depth
-    jmp .find_end
-
+    je   .dec_depth
+    jmp  .find_end
 .inc_depth:
     inc rcx
     jmp .find_end
-
 .dec_depth:
     dec rcx
     cmp rcx, 0
@@ -165,10 +193,11 @@ op_loop_start:
     jmp next
 
 op_loop_end:
-    cmpb [r15], 0
-    je next
+    ; ] : 現在セルが 0 でなければ対応する '[' の直後へ戻る
+    cmpb [tape_a+r15], 0   ; [SIB R_X86_64_32]
+    je   next
 
-    mov rcx, 1
+    mov rcx, 1             ; ネスト深度
 .find_start:
     cmp r14, 0
     jle exit
@@ -177,15 +206,13 @@ op_loop_end:
     add rax, r14
     movb al, [rax]
     cmpb al, ']'
-    je .inc_depth2
+    je   .inc_depth2
     cmpb al, '['
-    je .dec_depth2
-    jmp .find_start
-
+    je   .dec_depth2
+    jmp  .find_start
 .inc_depth2:
     inc rcx
     jmp .find_start
-
 .dec_depth2:
     dec rcx
     cmp rcx, 0
